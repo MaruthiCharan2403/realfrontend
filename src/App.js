842fc48e1d2505d9e6637a067bd0376f0853e424
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-03-22 21:15:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import PageNotFound from './Components/PageNotFound';
+import House from './Components/House';
+// import Enquiry from './Components/Enquiry';
+import Header from './Components/Header';
+import ListAHouse from './Components/ListAHouse';
+import Login from './Components/Login';
+import SearchHouse from './Components/SearchHouse';
+import SignUp from './Components/SignUp';
+import Home from './Components/Home';
+import Enquiries from './Components/Enquiries';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Welcome </h1>
+      <Header></Header>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="*" element={<PageNotFound/>} /> 
+          <Route path="house:id" element={<House/>} />
+          <Route path="listahouse" element={<ListAHouse/>} />
+          <Route path="login" element={<Login/>} />
+          <Route path='/searchhouse/:id' element={<SearchHouse/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/enquiries' element={<Enquiries/>}/>
+      </Routes>
     </div>
   );
 }
